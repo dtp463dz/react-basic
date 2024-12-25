cbe813dba79aa3de20e9a194e8d5f942eb805fb4
PhucDT <dinhthaiphuc463@gmail.com>
2024-12-26 00:36:41 +0700

    addState

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
 class MyComponent extends React.Component {
+    /**
+     * JSX => return block
+     * fragment
+     */
+    state = {
+        name: 'Dean',
+        school: 'PNK',
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
     render() {
         return (
-            <div> My component IT </div>
+            <>
+                <div className='first'>
+                    <input value={this.state.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                    Hello my component, my name is {this.state.name}
+                </div>
+
+                <div className='second'>
+                    My school: {this.state['school']}
+                </div>
+            </>
+
         )
     }
 }
