d1a7713c54f8d026927b1cd26629f0de550e2c1b
PhucDT <dinhthaiphuc463@gmail.com>
2025-01-14 14:45:10 +0700

    19scss_true

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Demo.scss';
 
 class ChildComponent extends React.Component {
     // re render
@@ -27,7 +28,10 @@ class ChildComponent extends React.Component {
             <>
                 {showJobs === false ?
                     <div>
-                        <button onClick={() => this.handleShowHide()}>Show</button>
+                        <button className="btn-show"
+                            onClick={() => this.handleShowHide()}>
+                            Show
+                        </button>
                     </div>
 
                     :
