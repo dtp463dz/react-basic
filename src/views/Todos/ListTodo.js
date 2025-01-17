97266cbdfb260c85302287a8bed9545780367aee
PhucDT <dinhthaiphuc463@gmail.com>
2025-01-17 23:55:31 +0700

    create_todoApp

--- src/views/Todos/ListTodo.js
+++ src/views/Todos/ListTodo.js
@@ -16,17 +16,16 @@ class ListTodo extends React.Component {
             <div className="list-todo-container">
                 <div className='add-todo'>
                     <input type="text" />
-                    <button type="button">Add</button>
+                    <button type="button" className='add'>Add</button>
                 </div>
-                <div> {24.46} </div>
                 <div className="list-todo-content">
                     {listTodos && listTodos.length > 0 &&
                         listTodos.map((item, index) => {
                             return (
-                                <div className="todo-child">
-                                    <span>Todo 1</span>
-                                    <button>Edit</button>
-                                    <button>Delete</button>
+                                <div className="todo-child" key={item.id}>
+                                    <span> {index + 1} - {item.title}</span>
+                                    <button className='edit'>Edit</button>
+                                    <button className='delete'>Delete</button>
                                 </div>
                             )
                         })
