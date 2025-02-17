ad92b653403fbcd43799950c505ffe61e5bf6c95
PhucDT <dinhthaiphuc463@gmail.com>
2025-02-17 21:39:06 +0700

    29ImportImage

--- src/views/Example/Home.js
+++ src/views/Example/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 // import { withRouter } from "react-router-dom";
 import Color from "../HOC/Color";
+import logo from '../../assets/images/hoa2.jpg';
 
 class Home extends React.Component {
     componentDidMount() {
@@ -12,9 +13,15 @@ class Home extends React.Component {
     render() {
         console.log('>>> check props: ', this.props)
         return (
-            <div>
-                Hello World From HomePage
-            </div>
+            <>
+                <div>
+                    Hello World From HomePage
+                </div>
+
+                <div>
+                    <img src={logo} style={{ width: '300px', height: '400px', marginTop: '20px' }} />
+                </div>
+            </>
         )
     }
 }
